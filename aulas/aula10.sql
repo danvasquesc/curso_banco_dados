8fb8f1a6c30c952e9369af378e0c29d47e02e84e
danvasquesc <dan.vasques@outlook.com.br>
2026-06-30 22:15:00 -0300

    Aula inicial sobre inner join

---
+++ aulas/aula10.sql
@@ -0,0 +1,30 @@
+-- AULA INICIAL SOBRE SQL JOINS
+
+use DB_T04301_DAN_VASQUES;
+
+CREATE TABLE TabelaA(
+  nome varchar(50) NULL);
+
+CREATE TABLE TabelaB(
+  nome varchar(50) NULL);
+
+INSERT INTO TabelaA 
+VALUES('Fernanda'),
+	  ('Josefa'),
+      ('Luiz'),
+      ('Fernando');
+
+INSERT INTO TabelaB 
+VALUES('Carlos'),
+	  ('Manoel'),
+      ('Luiz'),
+      ('Fernando');
+      
+select * from TabelaA;
+select * from TabelaB;
+
+-- INNER JOIN --
+-- retorna os registros/linhas que são comuns às duas tabelas
+select * from TabelaA as A
+inner join TabelaB as B
+on A.nome = B.nome;
